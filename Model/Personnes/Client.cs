4a8ce8f21b5439c703c3776a5859585fb244310d
DimitriLibessart <dimitri.libessart@viacesi.fr>
2018-12-05 19:47:22 +0100

    Add files via upload

---
+++ Model/Personnes/Client.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjetSalle
+{
+    class Client
+    {
+        int numTable;
+        int Commande = Commande;
+
+        public Client(int numTable)
+        {
+            this.numTable = numTable;
+        }
+    }
+}
---
+++ Model/Plat.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjetSalle
+{
+    class Plat
+    {
+        string Category;
+        int Price;
+        string Nom;
+        statutPlat = StatutPlat;
+        public Plat()
+        {
+          
+        }
+        void GetRecette(string nomRecette)
+        {
+          
+        }
+    }
+}
