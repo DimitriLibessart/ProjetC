b568036645dff31fed9acc3f54bed740fadc368b
Theo <theo.vanengelandt@viacesi.fr>
2018-12-10 09:41:42 +0100

    folder projet2 deleted and modifs add to the main project

--- ProjetSalle/ProjetSalle/Form1.cs
+++ ProjetSalle/ProjetSalle/Form1.cs
@@ -7,8 +7,10 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Threading;
+using System.Data.SqlClient;
 
-namespace ProjetSalle
+namespace Projet2
 {
     public partial class Form1 : Form
     {
@@ -16,20 +18,30 @@ namespace ProjetSalle
         {
             InitializeComponent();
         }
-
         private void Form1_Load(object sender, EventArgs e)
         {
-
+            Customer Michel = new Customer();
+            Michel.Arriver(pictureBox3);
+            HotelManager Denis = new HotelManager();
+            Denis.AccueilClient(talk);
+            
         }
-
-        private void textBox1_TextChanged(object sender, EventArgs e)
+        private void Valider_Click(object sender, EventArgs e)
         {
-
+            HotelManager Denis2 = new HotelManager();
+            Denis2.CheckReservation(talk,Nom,tbtxt2);
         }
 
-        private void textBox2_TextChanged(object sender, EventArgs e)
+        private void BtnPers_Click(object sender, EventArgs e)
+        {
+            HotelManager Denis3 = new HotelManager();
+            Denis3.AssignerTable(talk,tboxPers);
+        }
+
+        private void Testbtn_Click(object sender, EventArgs e)
         {
 
         }
     }
 }
+
--- ProjetSalle/ProjetSalle/Properties/Settings.Designer.cs
+++ ProjetSalle/ProjetSalle/Properties/Settings.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace ProjetSalle.Properties
+namespace Projet2.Properties
 {
 
 
