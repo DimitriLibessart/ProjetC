091c2d463a1fa29eaeb24328404827fd28b726cf
Corentin-Evrard <45556047+Corentin-Evrard@users.noreply.github.com>
2018-12-11 16:37:13 +0100

    CorrectionforMerge

--- ProjetSalle/ProjetSalle/Model/People/Customer.cs
+++ ProjetSalle/ProjetSalle/Model/People/Customer.cs
@@ -27,5 +27,46 @@ namespace ProjetSalle.Model.People
         {
             Console.WriteLine("Si ce n'est pas possible j'aimerais alors commander ceci : " + Command2); //Parse Commande2
         }
+        /*
+                 private Order order;
+        private List<Order> listOrder;
+        public Order(Order order, List<Order> listOrder)
+        {
+            try
+            {
+                TcpClient tcpclnt = new TcpClient();
+                Console.WriteLine("Connecting.....");
+
+                tcpclnt.Connect("127.0.0.1", 8001);
+                // use the ipaddress as in the server program
+
+                Console.WriteLine("Connected");
+                Console.Write("Enter the string to be transmitted : ");
+
+                String str = Console.ReadLine();
+                Stream stm = tcpclnt.GetStream();
+
+                ASCIIEncoding asen = new ASCIIEncoding();
+                byte[] ba = asen.GetBytes(str);
+                Console.WriteLine("Transmitting.....");
+
+                stm.Write(ba, 0, ba.Length);
+
+                byte[] bb = new byte[100];
+                int k = stm.Read(bb, 0, 100);
+
+                for (int i = 0; i < k; i++)
+                    Console.Write(Convert.ToChar(bb[i]));
+
+                tcpclnt.Close();
+            }
+
+            catch (Exception e)
+            {
+                Console.WriteLine("Error..... " + e.StackTrace);
+            }
+        }*/
+
     }
+    
 }
--- ProjetSalle/ProjetSalle/Model/People/RowChief.cs
+++ ProjetSalle/ProjetSalle/Model/People/RowChief.cs
@@ -8,38 +8,9 @@ using System.Windows.Forms;
 
 namespace ProjetSalle.Model.People
 {
-<<<<<<< HEAD
     class RowChief
+
     {
-        int IdTable { get; set; }
-        public void PlaceClient()
-        {
-            Console.WriteLine("Chef de Rang : Je vous place sur la table :" + IdTable);
-        }
-        public void PresenteCarte()
-        {
-            Console.WriteLine("Chef de Rang : Voici la carte");
-        }
-        public void MettreNappe()
-        {
-
-        }
-        public void PrendreCommande()
-        {
-
-        }
-        public void PrendreCommandePartie()
-        {
-
-        }
-        public void TransmetCommande()
-        {
-
-        }
-=======
-
-    class RowChief
-        {
             public void PlaceClient()
             {
             }
@@ -58,7 +29,7 @@ namespace ProjetSalle.Model.People
             public void TransmetCommande()
             {
             }
->>>>>>> d7df1a9e4bdcd4c9095cd8e995d711986453772a
+
     }
     
 
