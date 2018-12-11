612795ccb9c5ffa392d22ac7ff36232698d4f5db
Dimitri LIBESSART <dimitri.libessart@viacesi.fr>
2018-12-11 16:48:14 +0100

    serveurclass creation

--- ProjetSalle/ProjetSalle/Model/Order.cs
+++ ProjetSalle/ProjetSalle/Model/Order.cs
@@ -1,23 +1,127 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace ProjetSalle
+﻿
+public class Rootobject
 {
-    class Order
-    {
-        private Order order;
-        private List<Order> listOrder;
-        public Order(Order order, List<Order> listOrder)
-        {
-            this.order = order;
-            this.listOrder = listOrder;
-            //ConsoleKey;
-            //listePlat.Add(new Plat());
-        }
+public Commandsrestaurant CommandsRestaurant { get; set; }
+}
 
-    }
+public class Commandsrestaurant
+{
+public Salle1 Salle1 { get; set; }
+public Salle2 Salle2 { get; set; }
+}
 
+public class Salle1
+{
+public Command1 Command1 { get; set; }
+public Command2 Command2 { get; set; }
+}
+
+public class Command1
+{
+public string TableNumber { get; set; }
+public Commandslist CommandsList { get; set; }
+}
+
+public class Commandslist
+{
+public Client1 Client1 { get; set; }
+public Client2 Client2 { get; set; }
+}
+
+public class Client1
+{
+public string Entree { get; set; }
+public string Plat { get; set; }
+public string Dessert { get; set; }
+}
+
+public class Client2
+{
+public string Entree { get; set; }
+public string Plat { get; set; }
+public string Dessert { get; set; }
+}
+
+public class Command2
+{
+public string TableNumber { get; set; }
+public Commandslist1 CommandsList { get; set; }
+}
+
+public class Commandslist1
+{
+public Client11 Client1 { get; set; }
+public Client21 Client2 { get; set; }
+}
+
+public class Client11
+{
+public string Entree { get; set; }
+public string Plat { get; set; }
+public string Dessert { get; set; }
+}
+
+public class Client21
+{
+public string Entree { get; set; }
+public string Plat { get; set; }
+public string Dessert { get; set; }
+}
+
+public class Salle2
+{
+public Command11 Command1 { get; set; }
+public Command21 Command2 { get; set; }
+}
+
+public class Command11
+{
+public string TableNumber { get; set; }
+public Commandslist2 CommandsList { get; set; }
+}
+
+public class Commandslist2
+{
+public Client12 Client1 { get; set; }
+public Client22 Client2 { get; set; }
+}
+
+public class Client12
+{
+public string Entree { get; set; }
+public string Plat { get; set; }
+public string Dessert { get; set; }
+}
+
+public class Client22
+{
+public string Entree { get; set; }
+public string Plat { get; set; }
+public string Dessert { get; set; }
+}
+
+public class Command21
+{
+public string TableNumber { get; set; }
+public Commandslist3 CommandsList { get; set; }
+}
+
+public class Commandslist3
+{
+public Client13 Client1 { get; set; }
+public Client23 Client2 { get; set; }
+}
+
+public class Client13
+{
+public string Entree { get; set; }
+public string Plat { get; set; }
+public string Dessert { get; set; }
+}
+
+public class Client23
+{
+public string Entree { get; set; }
+public string Plat { get; set; }
+public string Dessert { get; set; }
 }
--- ProjetSalle/ProjetSalle/Model/People/Server.cs
+++ ProjetSalle/ProjetSalle/Model/People/Server.cs
@@ -9,11 +9,24 @@ namespace ProjetSalle.Model.People
     class Server
     {
         //private List<> listePorte;
+        public List<string> Commands { get; set; }
+        public Commandsrestaurant orderInst { get; set; }
+        
+        private Server()
+            {
+                orderInst = new Commandsrestaurant();
+                List<string> TmpOrderList = orderInst.Salle1.Command1;
+            
+                foreach (string Commande in TmpOrderList.CommandsList)
+                {
+                    foreach(string Recette in Commande)
+                    {
+                        Commands.Add(Recette);
+                        Console.WriteLine(Recette);
+                    }
+                }
 
-        public Server()
-        {
+            }
 
         }
-
-    }
 }
