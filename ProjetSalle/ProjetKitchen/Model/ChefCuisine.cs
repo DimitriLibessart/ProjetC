13ece51c2f3b683c3abeba6254176be9f31ab924
Corentin-Evrard <45556047+Corentin-Evrard@users.noreply.github.com>
2018-12-12 11:55:16 +0100

    Add Socket Command

--- ProjetSalle/ProjetKitchen/Model/ChefCuisine.cs
+++ ProjetSalle/ProjetKitchen/Model/ChefCuisine.cs
@@ -2,11 +2,35 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
 
 namespace ProjetKitchen.Model
 {
     class ChefCuisine
     {
+        string Str1 { get; set; }
+        bool Bool1 { get; set; }
+        public void ChangeStr(string Str1)
+        {
+            if (Str1 == "Bonjour")
+            {
+                Console.WriteLine("Le bonjour a été reçu");
+            }
+            else
+            {
+                Console.WriteLine("Erreur");
+            }
+        }
+        public void ChangeBool(bool Bool1)
+        {
+            Bool1 = false;
+            Console.WriteLine(Bool1);
+            Bool1 = true;
+            Console.WriteLine(Bool1);
+
+        }
         private static ChefCuisine _instance;
         static readonly object instanceLock = new object();
 
@@ -127,6 +151,32 @@ namespace ProjetKitchen.Model
                 }
                 return _instance;
             }
+        }
+        public void RecevoirCommande()
+        {
+
+            IPAddress ipAd = IPAddress.Parse("127.0.0.1");
+            TcpListener myListener = new TcpListener(ipAd, 8001);
+            myListener.Start();
+            Console.WriteLine("En attente d'une commande");
+            Socket s = myListener.AcceptSocket();
+            byte[] b = new byte[7];
+            int k = s.Receive(b);
+            char[] c = new char[7];
+            bool test = true;
+            for (int i = 0; i < k; i++)
+            {
+                c[i] = (Convert.ToChar(b[i]));
+            }
+            string Change = new string(c);
+            ASCIIEncoding asen = new ASCIIEncoding();
+            s.Send(asen.GetBytes("La commande a été reçue"));
+            s.Close();
+            myListener.Stop();
+            ChefCuisine Robert = new ChefCuisine();
+            Console.WriteLine(Change);
+            Robert.ChangeStr(Change);
+            Robert.ChangeBool(test);
         }
     }
 }
--- ProjetSalle/ProjetSalle/Model/People/RowChief.cs
+++ ProjetSalle/ProjetSalle/Model/People/RowChief.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Drawing;
+using System.IO;
 using System.Linq;
+using System.Net.Sockets;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -26,9 +28,30 @@ namespace ProjetSalle.Model.People
             public void PrendreCommandePartie()
             {
             }
-            public void TransmetCommande()
+        public void SendKitchen()
+        {
+            try
             {
+                TcpClient tcpclnt = new TcpClient();
+                tcpclnt.Connect("127.0.0.1", 8001);
+                String str = "Bonjour";
+                Stream stm = tcpclnt.GetStream();
+                ASCIIEncoding asen = new ASCIIEncoding();
+                byte[] ba = asen.GetBytes(str);
+                Console.WriteLine("Transmission de la commande en cuisine");
+                stm.Write(ba, 0, ba.Length);
+                byte[] bb = new byte[100];
+                int k = stm.Read(bb, 0, 100);
+                for (int i = 0; i < k; i++)
+                    Console.Write(Convert.ToChar(bb[i]));
+                tcpclnt.Close();
             }
+
+            catch (Exception e)
+            {
+                Console.WriteLine("Error..... " + e.StackTrace);
+            }
+        }
 
     }
     
