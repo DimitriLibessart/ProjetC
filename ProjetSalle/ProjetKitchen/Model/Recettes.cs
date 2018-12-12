35a91c05f2774ed6c5d70892c43cc7793f87ebb6
Corentin-Evrard <45556047+Corentin-Evrard@users.noreply.github.com>
2018-12-12 16:51:51 +0100

    Requete liste recettes

--- ProjetSalle/ProjetKitchen/Model/Recettes.cs
+++ ProjetSalle/ProjetKitchen/Model/Recettes.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 
 namespace ProjetKitchen.Model
 {
@@ -13,5 +14,64 @@ namespace ProjetKitchen.Model
 
         public List<string> ListIngredients { get; set; }
 
+        public void RecupRecette()
+        {
+            var NomEntree = new List<string>();
+            string Entree;
+            var NomPlat = new List<string>();
+            string Plat;
+            var NomDessert = new List<string>();
+            string Dessert;
+            using (SqlConnection conn = new SqlConnection())
+            {
+
+                conn.ConnectionString = "Server=ORDINATEUR-KISU\\SQLEXPRESS;Database=C#_Project;Trusted_Connection=true";
+                conn.Open();
+                SqlCommand command = new SqlCommand("SELECT Name FROM Recipe WHERE Category='Entree'", conn);
+                using (SqlDataReader reader = command.ExecuteReader())
+                {
+                    while (reader.Read())
+                    {
+                        Entree = string.Format("{0} \n",
+                            reader[0]);
+                        NomEntree.Add(Entree);
+                    }
+                }
+                SqlCommand command2 = new SqlCommand("SELECT Name FROM Recipe WHERE Category='Plat'", conn);
+                using (SqlDataReader reader = command2.ExecuteReader())
+                {
+                    while (reader.Read())
+                    {
+                        Plat = string.Format("{0} \n",
+                            reader[0]);
+                        NomPlat.Add(Plat);
+                    }
+                }
+                SqlCommand command3 = new SqlCommand("SELECT Name FROM Recipe WHERE Category='Dessert'", conn);
+                using (SqlDataReader reader = command3.ExecuteReader())
+                {
+                    while (reader.Read())
+                    {
+                        Dessert = string.Format("{0} \n",
+                            reader[0]);
+                        NomDessert.Add(Dessert);
+                    }
+                }
+                Console.ReadLine();
+                foreach (var item in NomEntree)
+                {
+                    Console.WriteLine(item);
+                }
+                foreach (var item in NomPlat)
+                {
+                    Console.WriteLine(item);
+                }
+                foreach (var item in NomDessert)
+                {
+                    Console.WriteLine(item);
+                }
+            }
         }
+
+    }
 }
